b767d0445028d794a885234eeec77d40b54ae3c8
valsev99 <valhag03@edu.umea.se>
2026-09-17 13:41:02 +0200

    början

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-# Här skriver du ditt textäventyr
+# handlar på willis
+
+bil = print ("Du ska handla på Willis du ska välja vilken bil du ska åka med klicka 1 för din monster truck klicka 2 för din golfbil och klicka 3 för din familjebil")
+
+if bil == 1:
+    print("")
